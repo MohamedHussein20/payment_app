f4540fe18bf8931e2854b524017295ae7c578e68
Mohamed Hussein <mohussein1279@gmail.com>
2025-11-11 12:18:20 +0200

    create app styles

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+abstract class Styles {
+  static TextStyle style25 = TextStyle(
+    fontSize: 25.sp,
+    color: Colors.black,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    height: 8.h,
+  );
+  static TextStyle style24 = TextStyle(
+    fontSize: 25.sp,
+    color: Colors.black,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 8.h,
+  );
+  static TextStyle style18 = TextStyle(
+    fontSize: 18.sp,
+    color: Colors.black,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+  static TextStyle styleSemiBold18 = TextStyle(
+    fontSize: 18.sp,
+    color: Colors.black,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  static TextStyle style20 = TextStyle(
+    fontSize: 20.sp,
+    color: Colors.black,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+}
