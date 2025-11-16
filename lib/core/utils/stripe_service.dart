0c46598035f54ae817bab1032fa07dda29100503
Mohamed Hussein <mohussein1279@gmail.com>
2025-11-16 16:10:03 +0200

    create api service

---
+++ lib/core/utils/stripe_service.dart
@@ -0,0 +1,7 @@
+import 'package:payment_app/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
+
+class StripeService {
+  PaymentIntentModel createPaymentIntent(PaymentIntentModel paymentIntentModel) {
+       
+  }
+}
