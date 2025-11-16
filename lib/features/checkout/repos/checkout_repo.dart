c74f9013554cc71947ddda3470167d020ae34e28
Mohamed Hussein <mohussein1279@gmail.com>
2025-11-16 22:15:39 +0200

    create checkout repo

---
+++ lib/features/checkout/repos/checkout_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:payment_app/core/utils/errors.dart';
+import 'package:payment_app/features/checkout/data/models/payment_intent_input_model.dart';
+
+abstract class CheckoutRepo {
+  Future<Either<Failure, void>> checkout({
+    required PaymentIntentInputModel paymentIntentInputModel,
+  });
+}
